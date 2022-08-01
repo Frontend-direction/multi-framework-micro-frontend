436f1e5d5ee6cd5c6b3cd544863d30acf6776091
volodymyrpankiv <volodymyr.pankiv@teck.com>
2022-08-01 10:34:53 +0300

    change structure of components

--- board-app/src/App.vue
+++ board-app/src/App.vue
@@ -1,37 +1,53 @@
 <template>
   <div id="app">
-    <div class="mb-3">
+    <div class="mb-2">
       <span class="label label-primary">Vue v {{ version }}</span>
     </div>
-   <Board />
+   
+    <div class="container bs-docs-container">
+      <div class="row">
+        <div class="col-xs-6 col-md-3" v-for="app in [1,2,3, 4, 5, 6, 7, 8, 9]" :key="app">
+          <div class="thumbnail">
+            <img :src="cardImg" alt="...">
+            <div class="caption">
+              <h3>New Micro frontend app</h3>
+              <p>Some fancy description</p>
+              <p>
+                <a href="#" class="btn btn-primary mr-2" role="button">Launch</a>
+                <a href="#" class="btn btn-default" role="button">Info</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import Board from './components/Board.vue';
-
 export default {
   name: 'App',
-  components: {
-    Board,
-  },
   data() {
     return {
-      version: '3.2.13'
+      version: '3.2.13',
+      cardImg: require('./assets/card.svg')
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 2rem;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 2rem;
+  }
 
-.mb-3 { margin-bottom: 5rem;}
+  .mb-2 { margin-bottom: 5rem;}
+
+  .mr-2 { margin-right:.5rem; }
 </style>
--- chart-app/src/root.component.js
+++ chart-app/src/root.component.js
@@ -1,15 +1,21 @@
-import { Chart } from './Chart';
-import './style.css';
+import "./style.css";
+import chart1 from "./assets/chart1.svg";
+import chart2 from "./assets/chart2.svg";
+import chart3 from "./assets/chart3.svg";
 
 export default function Root(props) {
-  const version = '17.0.2';
+  const version = "17.0.2";
 
   return (
     <section>
-    <div class="mb-3 mt-1 text-center">
-      <span class="label label-primary">React v { version }</span>
-    </div>
-      <Chart />
+      <div class="mb-3 mt-1 text-center">
+        <span class="label label-primary">React v {version}</span>
+      </div>
+      <div>
+        <img src={chart1} alt="chart1" />
+        <img src={chart2} alt="chart1" />
+        <img src={chart3} alt="chart1" />
+      </div>
     </section>
   );
 }
